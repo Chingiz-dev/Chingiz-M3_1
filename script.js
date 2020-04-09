1dff0f38438817f21611b77218189798e0dae4ef
Chingiz Shigayev <shigayevphone@gmail.com>
2020-04-09 19:31:26 +0400

    final

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-const draggable = document.querySelector('.input-container');
-const container = document.querySelector('.container');
+const addDrag = (e) => {
+    e.target.parentElement.classList.add('dragging');
+}
 
-
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging');
-    });
-
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging');
-    });
-
-
-
-container.addEventListener('dragover', e => {
-    e.preventDefault()
-    const afterElement = getDragAfterElement(container, e.clientY)
-    const draggable = document.querySelector('.dragging')
-    if (afterElement == null) {
-        container.appendChild(draggable)
-    } else {
-        container.insertBefore(draggable, afterElement)
-    }
-})
-
+const removeDrag = (e) => {
+    e.target.parentElement.classList.remove('dragging');
+}
 
 function getDragAfterElement(container, y) {
     const draggableElements = [...container.querySelectorAll('.input-container:not(.dragging)')]
-
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
         const offset = y - box.top - box.height / 2
@@ -81,21 +62,23 @@ const deliteContainer = (e) => {
     toDel.remove();
 }
 
-const builNewInputContainer = () => {
+const buildNewInputContainer = () => {
     const inputContainer = document.createElement('div');
     inputContainer.className = 'input-container';
-    inputContainer.draggable = 'true';
-    inputContainer.addEventListener('dragstart', () => {
-        inputContainer.classList.add('dragging');
-    })
+    // inputContainer.addEventListener('dragstart', () => {
+    //     inputContainer.classList.add('dragging');
+    // })
 
-    inputContainer.addEventListener('dragend', () => {
-        inputContainer.classList.remove('dragging');
-    })
-    container.append(inputContainer);
+    // inputContainer.addEventListener('dragend', () => {
+    //     inputContainer.classList.remove('dragging');
+    // })
+     container.append(inputContainer);
 
     const mover = document.createElement('div');
+    mover.draggable = 'true';
     mover.className = 'mover';
+    mover.addEventListener('dragstart', addDrag);
+    mover.addEventListener('dragend', removeDrag);
     mover.innerHTML = '&#8759';
     inputContainer.append(mover);
 
@@ -109,15 +92,25 @@ const builNewInputContainer = () => {
     inputContainer.append(deliter);
 }
 
+const container = document.querySelector('.container');
+container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+        container.appendChild(draggable)
+    } else {
+        container.insertBefore(draggable, afterElement)
+    }
+})
+
 const firstContainerDelete = document.querySelector('.deliter');
-firstContainerDelete.addEventListener('click', deliteContainer);
-
-
 const sorter = document.getElementById('sorter');
-sorter.addEventListener('click', sortText);
-
 const newInput = document.querySelector('.new-input');
-newInput.addEventListener('click', builNewInputContainer);
+const draggable = document.querySelector('.mover');
 
-
-
+firstContainerDelete.addEventListener('click', deliteContainer);
+newInput.addEventListener('click', buildNewInputContainer);
+sorter.addEventListener('click', sortText);
+draggable.addEventListener('dragstart', addDrag);
+draggable.addEventListener('dragend', removeDrag);
